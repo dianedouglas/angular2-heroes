c628aa9135a5ff067d89c2ecb5e2c185ed8d47c7
Diane <diane.douglas1@gmail.com>
2016-09-04 11:03:48 -0400

    Display basic data in the app with one way databinding on properties following basic typescript declaration syntax.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>My First Angular 2 App</h1>
+  <h1>My Favorite Things</h1>
+  <ul>
+    <li>One of my favorite bands is: {{favoriteBand}}</li>
+    <li>If I had to choose a favorite painter it would be: {{favoritePainter}}</li>
+    <li>The number of slices of pie I would like is: {{slicesOfPie}}</li>
+  </ul>
   `
 })
 
-export class AppComponent { }
+export class AppComponent { 
+  favoriteBand: string = 'Jimi Hendrix';
+  favoritePainter: string = 'Van Gogh';
+  slicesOfPie: number = 3;
+}
