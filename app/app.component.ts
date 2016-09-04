98dcfe2035392f56688465120d70b6470555428d
Diane <diane.douglas1@gmail.com>
2016-09-04 11:14:43 -0400

    You can load objects as data too. We view their properties in our template using dot notation.

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,6 +9,10 @@ import { Component } from '@angular/core';
     <li>If I had to choose a favorite painter it would be: {{favoritePainter}}</li>
     <li>The number of slices of pie I would like is: {{slicesOfPie}}</li>
   </ul>
+  <h3>One of my favorite albums is: </h3>
+  <p>{{favoriteAlbum.title}}</p>
+  <p>By {{favoriteAlbum.artist}}</p>
+  <p>Released in {{favoriteAlbum.released}}</p>
   `
 })
 
@@ -16,4 +20,15 @@ export class AppComponent {
   favoriteBand: string = 'Jimi Hendrix';
   favoritePainter: string = 'Van Gogh';
   slicesOfPie: number = 3;
+  favoriteAlbum: Album = {
+    title: "Disintegration",
+    artist: "The Cure",
+    released: 1989
+  }
+}
+
+export class Album {
+  title: string;
+  artist: string;
+  released: number;
 }
